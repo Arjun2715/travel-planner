c466509914e147070163ad273613d98a9115a70f
Arjun2715 <arjunjot18@gmail.com>
2025-02-06 12:28:13 +0100

    minorchanges

--- components/Chat-rightside.tsx
+++ components/Chat-rightside.tsx
@@ -1,28 +1,22 @@
 import React, { Component } from 'react'
-
 type Props = {}
-
 type State = {}
-
 export class ChatIntrorightside extends Component<Props, State> {
   state = {}
-
   render() {
     return (
       <div
         className="flex-1 bg-cover bg-center rounded-lg"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')`,
-          height: "400px", // Set a fixed height for the image container
+          height: "400px",
         }}
       > 
-        <div className="absolute w-full h-full inset-0 flex flex-col justify-between p-6">
-          {/* Top Text (h1) */}
+        <div className="absolute w-full h-full inset-0 flex flex-col justify-between p-6"> 
           <h1 className="text-xl md:text-7xl  mt-20 font-bold text-white text-center">
             Your Next Travel Adventure
           </h1>
-          <div className="flex flex-col justify-center align-middle">
-            {/* Bottom Text (p) */}
+          <div className="flex flex-col justify-center align-middle"> 
             <p className="text-xl text-white text-center">
               Got a vacation coming up? Start here by asking anything about it.
             </p>
@@ -39,8 +33,8 @@ export class ChatIntrorightside extends Component<Props, State> {
               <g id="SVGRepo_iconCarrier">
                 <path
                   d="M150 12H4M4 12L10 6M4 12L10 18"  
-                  stroke="#ffffff" // Arrow color
-                  strokeWidth="1.5" // Increase stroke width for better visibility
+                  stroke="#ffffff"
+                  strokeWidth="1.5"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
